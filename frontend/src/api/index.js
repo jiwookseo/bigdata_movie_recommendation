e59e54b705baa8910d4c352ad145de486a4cfd8f
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-08-29 10:56:41 +0900

    [수정] REST api 수정에 따른 frontend api 호출 및 vuex store 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,21 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
-const apiUrl = '/api';
 const BASE_URL = "/api";
-
 
 export default {
   searchMovies(params) {
-    return axios.get(`${apiUrl}/movies/?title=`, {
-      params,
-    })
+    return axios.get(`${BASE_URL}/movies/`, {
+      params
+    });
   },
-
-  get_moviesKey(params) {
-    return axios.get(BASE_URL + "/movies/?", {params})
+  getMovie(id) {
+    return axios.get(`${BASE_URL}/movies/${id}/`);
   },
-
-  get_usersKey(params) {
-    return axios.get(BASE_URL + "/auth/signup-many/?", {params})
+  getAudience(id) {
+    return axios.get(`${BASE_URL}/movies/${id}/ratings/`);
+  },
+  searchUsers(params) {
+    return axios.get(`${BASE_URL}/users/`, {
+      params
+    });
+  },
+  getUser(username) {
+    return axios.get(`${BASE_URL}/users/${username}`);
+  },
+  getRatings(username) {
+    return axios.get(`${BASE_URL}/users/${username}/ratings/`);
   }
-}
+};
--- frontend/src/store/modules/data.js
+++ frontend/src/store/modules/data.js
@@ -1,88 +1,91 @@
-import api from '../../api'
+import api from "../../api";
 
 // initial state
 const state = {
   // shape: [{ id, title, genres, viewCnt, rating }]
   movieSearchList: [],
   movieSelected: [],
+  audience: [],
   userSearchList: [],
   userSelected: [],
-}
+  ratings: []
+};
+
+// getters
+const getters = {
+  movieSearchList: state => state.movieSearchList,
+  movieSelected: state => state.movieSelected,
+  audience: state => state.audience,
+  userSearchList: state => state.userSearchList,
+  userSelected: state => state.userSelected,
+  ratings: state => state.ratings
+};
 
 // actions
 const actions = {
-  async get_moviesKey({ commit }, params) {
-    const resp = await api.get_moviesKey(params);
+  // Movie
+  async getMovieById({ commit }, id) {
+    const resp = await api.getMovie(id);
+    const movie = {
+      id: resp.data.id,
+      title: resp.data.title,
+      genres: resp.data.genres_array,
+      viewCnt: resp.data.rating_count,
+      rating: resp.data.avg_rating
+    };
+    commit("setMovie", movie);
+    const res = await api.getPeople(id);
+    const audience = res.data.map(d => d.username);
+    commit("setAudience", audience);
+  },
+  async searchMovies({ commit }, params) {
+    const resp = await api.searchMovies(params);
     const movies = resp.data.map(d => ({
       id: d.id,
       title: d.title,
       genres: d.genres_array,
       viewCnt: d.rating_count,
-      rating: d.avg_rating,
+      rating: d.avg_rating
     }));
-    commit('setMovieSearchList', movies)
+
+    commit("setMovieSearchList", movies);
+  },
+  setEmptyMovieList({ commit }) {
+    commit("setMovieSearchList", []);
   },
 
-  async get_movieKey({ commit }, params) {
-    const resp = await api.get_moviesKey(params);
-    const movie = resp.data.map(d => ({
-      id: d.id,
-      title: d.title,
-      genres: d.genres_array,
-      viewCnt: d.rating_count,
-      rating: d.avg_rating,
-      story: d.story,
-      ratings: d.ratings
-    }));
-    commit("setMovieSelected", movie)
+  // User
+  async searchUsers({ commit }, params) {
+    const resp = await api.searchUsers(params);
+    const users = resp.data;
+    commit("setUserSearchList", users);
   },
-  async get_usersKey({ commit }, params) {
-    const resp = await api.get_usersKey(params);
-    const users = resp.data.map(d => ({
-      id: d.id,
-      username: d.username,
-      is_staff: d.is_staff,
-      gender: d.gender,
-      age: d.age,
-      occupation: d.occupation,
-    }));
-    commit('setUserSearchList', users)
+  async getUserByUsername({ commit }, username) {
+    const res1 = await api.getUser(username);
+    const user = res1.data;
+    commit("setUserSelected", user);
+    const res2 = await api.getRatings(username);
+    commit("setRatings", res2.data);
   },
-
-  async get_userKey({ commit }, params) {
-    const resp = await api.get_usersKey(params);
-    const user = resp.data.map(d => ({
-      id: d.id,
-      username: d.username,
-      is_staff: d.is_staff,
-      gender: d.gender,
-      age: d.age,
-      occupation: d.occupation,
-      ratings: d.ratings
-    }));
-    commit("setUserSelected", user)
+  setEmptyUserList({ commit }) {
+    commit("setUserSearchList", []);
   }
 };
 
 // mutations
 const mutations = {
-  setMovieSearchList(state, movies) {
-    state.movieSearchList = movies.map(m => m)
-  },
-  setMovieSelected(state, movie) {
-    state.movieSelected = movie.map(m => m)
-  },
-  setUserSearchList(state, users) {
-    state.userSearchList = users.map(m => m)
-  },
-  setUserSelected(state, user) {
-    state.userSelected = user.map(m => m)
-  }
+  setMovieSearchList: (state, payload) => (state.movieSearchList = payload),
+  setMovieSelected: (state, payload) => (state.movieSelected = payload),
+  setUserSearchList: (state, payload) => (state.userSearchList = payload),
+  setUserSelected: (state, payload) => (state.userSelected = payload),
+  setRatings: (state, payload) => (state.ratings = payload),
+  setAudience: (state, payload) => (state.audience = payload)
 };
 
 export default {
   namespaced: true,
   state,
   actions,
-  mutations
-}
+  mutations,
+  getters
+};
