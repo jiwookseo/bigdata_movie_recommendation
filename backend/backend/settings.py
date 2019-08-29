81fdac95697cfea9de4adcb94bfc5347990847dc
이지선 <yjs1819@naver.com>
2019-08-29 01:06:28 +0000

    Revert "[추가] JWT 인증 관련 세팅 추가"
    This reverts commit 4a8938cbd17e1d4f58b23fc6a80d24e232eb29b0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,30 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# JWT Authentication
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
-
-JWT_AUTH = {
-    'JWT_SECRET_KEY': SECRET_KEY,
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=28),
-}
-
-
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
