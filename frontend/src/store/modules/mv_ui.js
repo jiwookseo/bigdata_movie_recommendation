d222e9bb9c9d931fc2a9f8efeb9e5905f6c216f5
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-09-02 16:36:58 +0900

    [추가] Vuex 에 imgSlider 관련 state 추가

--- frontend/src/store/modules/mv_ui.js
+++ frontend/src/store/modules/mv_ui.js
@@ -1,21 +1,36 @@
 const state = {
   detailToggler: false,
-  activateMovie: {},
-}
+  detailType: "",
+  activateMovie: {}
+};
 
 const getters = {
-  movie: state => state.activateMovie
-}
+  movie: state => state.activateMovie,
+  detailToggler: state => state.detailToggler,
+  detailType: state => state.detailType
+};
+
+const actions = {
+  setDetailToggler({ commit }, type = "") {
+    if (state.detailToggler && state.detailType !== type) {
+      commit("setDetailType", type);
+    } else {
+      commit("setDetailType", type);
+      commit("setDetailToggler");
+    }
+  }
+};
 
 const mutations = {
   setDetailToggler: state => (state.detailToggler = !state.detailToggler),
   setActivateMovie: (state, payload) => (state.activateMovie = payload),
-}
-
+  setDetailType: (state, payload) => (state.detailType = payload)
+};
 
 export default {
   namespaced: true,
   state,
+  actions,
   mutations,
   getters
-}
+};
