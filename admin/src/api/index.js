ec273cd0120454648097103c2859d62706cc5d2a
Sungjik <sungjik6875@naver.com>
2019-09-03 13:36:57 +0900

    [수정] 관리자 페이지 유저, 영화 클러스터링 테스트 완료

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -3,14 +3,28 @@ import axios from 'axios'
 const apiUrl = '/api'
 
 export default {
+  // Movies
   searchMovies(params) {
     return axios.get(`${apiUrl}/movies/`, {
       params,
     })
   },
   clusteringMovies(params) {
-    return axios.post(`${apiUrl}/clustering/movies/`, {
+    return axios.post(`${apiUrl}/clustering/movies/`, 
+      params,
+    )
+  },
+  getRelatedMovies(params) {
+    return axios.get(`${apiUrl}/movies/related_movies/`, {
       params,
     })
+  },
+
+
+  // Users
+  clusteringUsers(params) {
+    return axios.post(`${apiUrl}/clustering/users/`, 
+      params,
+    )
   }
 }
