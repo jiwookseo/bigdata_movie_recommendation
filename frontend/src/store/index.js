0018cf5d601ed858e5ccd7b88751f5e5e204a141
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-09-09 17:50:12 +0900

    [수정] data store user / movie 로 분리

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import data from "./modules/data";
+import user from "./modules/user";
+import movie from "./modules/movie";
 import mvUi from "./modules/mv_ui";
 import snackbar from "./modules/snackbar";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    data,
+    user,
+    movie,
     mvUi,
     snackbar
   }
