81b2858b967d69f0c81eb4e353d1baa92c4c3ea7
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-09-10 17:07:57 +0900

    [추가] 유저 프로필 사진 및 썸네일 추가

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFit
+
+
+def image_path(instance, filename):
+    return 'profile/{}/{}'.format(instance.id, filename)
 
 
 class User(AbstractUser):
     gender = models.CharField(max_length=10, default='M')
     age = models.IntegerField(default=25)
     occupation = models.CharField(max_length=200)
-    cluster = models.IntegerField(null=True)
+    cluster = models.IntegerField(blank=True)
     refresh_token = models.TextField(default="")
+    image = ProcessedImageField(
+        upload_to=image_path,
+        processors=[ResizeToFit(width=150, upscale=False)],
+        format='JPEG',
+        blank=True
+    )
+    thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFit(width=50, upscale=False)],
+        format='JPEG',
+        options={'quality': 60},
+    )
 
     def __str__(self):
         return "{}".format(self.username)
