5c5208ba57901592acb2a0b28ae01a4f76dabfc4
jiwookseo <jiwookseo.dev@gmail.com>
2019-10-08 07:28:34 +0000

    [추가] 배포환경 설정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   publicPath: "/",
   devServer: {
-    proxy: {
-      "/api": {
-        target: "http://52.78.81.59:8000/"
-      },
-      "/static/posters": {
-        target: "http://52.78.81.59:8000/"
-      }
     }
-  }
 };
